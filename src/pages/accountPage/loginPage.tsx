5f36966491d667912dd0f04d9cf4755c49be3367
Леха <znaxor74@mail.ru>
2024-05-07 10:03:25 +0300

    RSS-ECOMM-2_01/feat: implement the start layout

--- src/pages/accountPage/loginPage.tsx
+++ src/pages/accountPage/loginPage.tsx
@@ -1,32 +1,51 @@
 import './loginPage.scss';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import MyButton from '../../components/button/button';
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
 
-function loginPage() {
+function AccountPage() {
+  const {
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
-    <div className="greyWrapper greyWrapper_loginFiled">
-      <form className="loginForm">
-        <fieldset className="loginForm__fieldset">
-          <legend className="loginForm__legend">Already a customer?</legend>
-          <label className="loginForm__label">Welcome back! Sign in for faster checkout.</label>
-          <input className="emailInput" type="email" placeholder="Email Address"></input>
-          <input
-            className="passwordInput"
-            type="password"
-            placeholder="Enter your password"
-          ></input>
-          <label className="rememberMe__label" htmlFor="rem">
-            {' '}
+    <div className="authorization-field">
+      <form onSubmit={handleSubmit(onSubmit)} className="authorization-field_form login-form">
+        <fieldset className="login-form_fieldset">
+          <legend className="login-form_legend">Already a customer?</legend>
+          <label className="login-form_label">Welcome back! Sign in for faster checkout.</label>
+          <div className="login-form_email-input-container">
+            <input
+              className="login-form_email-input"
+              type="email"
+              placeholder="Email Address"
+            ></input>
+          </div>
+          <div className="login-form_password-input-container">
+            <input
+              className="login-form_password-input"
+              type="password"
+              placeholder="Enter your password"
+            ></input>
+          </div>
+          <label className="login-form_remember-Label" htmlFor="rem">
+            <a href="#" className="login-form_link">
+              {' '}
+              Forget password?
+            </a>{' '}
             Please remember me
+            <input className="login-form_remember-Input" type="checkbox" id="rem"></input>
           </label>
-          <input className="rememberMe__input" type="checkbox" id="rem"></input>
-          <a href="#" className="forget">
-            Forget password?
-          </a>
-          <MyButton text="SIGN IN" className="button" type="submit" />
+          <MyButton text="Sign in" type="submit" />
         </fieldset>
       </form>
     </div>
   );
 }
 
-export default loginPage;
+export default AccountPage;
