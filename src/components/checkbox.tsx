80b1064080574f9140f9ca49b6cf69ba50065a8c
Anton <lipan48@gmail.com>
2024-05-08 20:11:55 +0300

    RSS-ECOMM-0/feat: add checkbox and checkbox slide

---
+++ src/components/checkbox.tsx
@@ -0,0 +1,10 @@
+function Checkbox() {
+  return (
+    <label className='checkbox-label'>
+      <input type="checkbox" className='checkbox-input' />
+      <div className='checkbox-custom'></div>
+    </label>
+  );
+}
+
+export default Checkbox;
