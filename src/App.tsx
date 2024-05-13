3fac95581e29023a83309bd830f539aca7e9fcc3
Anton <lipan48@gmail.com>
2024-05-13 12:55:00 +0300

    RSS-ECOMM-2_18/feat: link to login page

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import AboutUs from './pages/aboutUs/aboutUs';
 import Collection from './pages/collections/collection';
-import RegistrationForm from './pages/registrationPage/registrationPage';
 import MyBag from './pages/myBag/myBag';
+import AccountPage from './pages/accountPage/loginPage';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="about" element={<AboutUs />} />
         <Route path="collection" element={<Collection />} />
-        <Route path="account" element={<RegistrationForm />} />
+        <Route path="account" element={<AccountPage />} />
         <Route path="bag" element={<MyBag />} />
       </Routes>
       <Footer />
