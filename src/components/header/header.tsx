f54f29b445d106353deba486c0aa4a9ba4b23eb5
Anton <lipan48@gmail.com>
2024-05-14 23:31:04 +0300

    RSS-ECOMM-2_20_1/feat: adjust login and singin buttons in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import './header.scss';
 import logoSearch from '../../assets/svg/icon-search.svg';
-import logoUser from '../../assets/svg/icon-person.svg';
+// import logoUser from '../../assets/svg/icon-person.svg';
 import logoCart from '../../assets/svg/icon-local_mall.svg';
 
 function Header() {
@@ -12,23 +12,36 @@ function Header() {
           Monkey Tea
         </Link>
         <nav className="nav">
-          <Link className="nav_link btn_blank" to="/collection">
-            tea collection
-          </Link>
-          <Link className="nav_link btn_blank" to="/about">
-            about us
-          </Link>
+          <div className="page-links">
+            <Link className="nav_link btn_blank" to="/collection">
+              tea collection
+            </Link>
+            <Link className="nav_link btn_blank" to="/about">
+              about us
+            </Link>
+          </div>
+          <div className="user-btns">
+            <Link className="user-btns_btn" to="">
+              <img className="user-btns_btn__icon" src={logoSearch} />
+            </Link>
+            {/* <Link className="user-btns_btn" to="account">
+              <img className="user-btns_btn__icon" src={logoUser} />
+            </Link> */}
+            <Link className="user-btns_btn" to="bag">
+              <img className="user-btns_btn__icon" src={logoCart} />
+            </Link>
+            <Link className="btn_white btn_header" to="account">
+              Log In
+            </Link>
+            <Link className="btn_black btn_header" to="registrtion">
+              Sing In
+            </Link>
+          </div>
         </nav>
-        <div className="user-btns">
-          <Link className="user-btns_btn" to="">
-            <img className="user-btns_btn__icon" src={logoSearch} />
-          </Link>
-          <Link className="user-btns_btn" to="account">
-            <img className="user-btns_btn__icon" src={logoUser} />
-          </Link>
-          <Link className="user-btns_btn" to="bag">
-            <img className="user-btns_btn__icon" src={logoCart} />
-          </Link>
+        <div className="burger">
+          <div className="burger_line top"></div>
+          <div className="burger_line mid"></div>
+          <div className="burger_line bot"></div>
         </div>
       </header>
     </>
