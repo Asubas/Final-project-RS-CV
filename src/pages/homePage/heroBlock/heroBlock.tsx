d5ed043d0ba879f23f83183a8e6250c9a31d1946
Anton <lipan48@gmail.com>
2024-05-16 14:17:48 +0300

    RSS-ECOMM-2_20_1/feat: implement responsive layout for hero

--- src/pages/homePage/heroBlock/heroBlock.tsx
+++ src/pages/homePage/heroBlock/heroBlock.tsx
@@ -1,22 +1,23 @@
 import './heroBlock.scss';
-import heroImg from '../../../assets/img/hero.jpg';
 import { Link } from 'react-router-dom';
 
 function HeroBlock() {
   return (
     <>
-      <section className="heroWrap">
-        <img src={heroImg} alt="Different teas" className="heroImg" />
-        <div className="heroDisc">
-          <h1 className="heroDisc_h1">Every day is unique, just like our tea</h1>
-          <p className="heroDisc_text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae dolores tenetur
-            doloremque, repudiandae harum magni, praesentium veniam dicta officia pariatur sunt iure
-            sequi, molestiae enim blanditiis rerum culpa nam!
-          </p>
-          <Link className="btn_black heroDisc_link" to="collection">
-            browes teas
-          </Link>
+      <section className="heroSection">
+        <div className="heroWrap">
+          <div className="heroImgDiv"></div>
+          <div className="heroDisc">
+            <h1 className="heroDisc_h1">Every day is unique, just like our tea</h1>
+            <p className="heroDisc_text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quae dolores
+              tenetur doloremque, repudiandae harum magni, praesentium veniam dicta officia ariatur
+              sunt iure sequi, molestiae enim blanditiis rerum culpa nam!
+            </p>
+            <Link className="btn_black heroDisc_link" to="collection">
+              browes teas
+            </Link>
+          </div>
         </div>
       </section>
     </>
