fb00596abd785b623452fb8af6d9297e684ef251
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-05-17 10:02:48 +0200

    RSS-ECOMM-2_11/feat: realization connection with commers tools

--- src/components/getUserObjectRegistrationPage/getUserObjectRegistrationPage.tsx
+++ src/components/getUserObjectRegistrationPage/getUserObjectRegistrationPage.tsx
@@ -1,45 +1,55 @@
-async function getUserObjectRegistrationPage(){
-    const firstNameInput = document.querySelector('.registration-form_first-name-input') as HTMLInputElement;
-    const lastNameInput = document.querySelector('.registration-form_last-name-input') as HTMLInputElement;
-    const emailInput = document.querySelector('.registration-form_email-input') as HTMLInputElement;
-    const passwordInput = document.querySelector('.registration-form_password-input') as HTMLInputElement;
-    const countrySelect = document.querySelector('.custom-select__input-container');
-    const cityInput = document.querySelector('.registration-form_city-input') as HTMLInputElement;
-    const streetInput = document.querySelector('.registration-form_street-input') as HTMLInputElement;
-    const postalCodeInput = document.querySelector('.registration-form_postal-code-input') as HTMLInputElement;
-    const dateOfBirthInput = document.querySelector('.registration-form_date-of-birth-input') as HTMLInputElement;
+async function getUserObjectRegistrationPage() {
+  const firstNameInput = document.querySelector(
+    '.registration-form_first-name-input',
+  ) as HTMLInputElement;
+  const lastNameInput = document.querySelector(
+    '.registration-form_last-name-input',
+  ) as HTMLInputElement;
+  const emailInput = document.querySelector('.registration-form_email-input') as HTMLInputElement;
+  const passwordInput = document.querySelector(
+    '.registration-form_password-input',
+  ) as HTMLInputElement;
+  const countrySelect = document.querySelector('.custom-select__input-container');
+  const cityInput = document.querySelector('.registration-form_city-input') as HTMLInputElement;
+  const streetInput = document.querySelector('.registration-form_street-input') as HTMLInputElement;
+  const postalCodeInput = document.querySelector(
+    '.registration-form_postal-code-input',
+  ) as HTMLInputElement;
+  const dateOfBirthInput = document.querySelector(
+    '.registration-form_date-of-birth-input',
+  ) as HTMLInputElement;
 
+  console.log(firstNameInput.value);
+  console.log(lastNameInput.value);
+  console.log(emailInput.value);
+  console.log(passwordInput.value);
+  console.log(countrySelect);
+  console.log(cityInput.value);
+  console.log(streetInput.value);
+  console.log(postalCodeInput.value);
+  console.log(dateOfBirthInput.value);
 
-    console.log(firstNameInput.value)
-    console.log(lastNameInput.value)
-    console.log(emailInput.value)
-    console.log(passwordInput.value)
-    console.log(countrySelect)
-    console.log(cityInput.value)
-    console.log(streetInput.value)
-    console.log(postalCodeInput.value)
-    console.log(dateOfBirthInput.value)
+  const userRegistrationData = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
 
-    const userRegistrationData = {
-        "firstName": firstNameInput.value,
-        "lastName": lastNameInput.value,
-        "email": emailInput.value,
-        "password": passwordInput.value
-    }
-
-    const response = await fetch("https://api.europe-west1.gcp.commercetools.com/coffee-tea-shop/custemers/",
+  const response = await fetch(
+    'https://api.europe-west1.gcp.commercetools.com/coffee-tea-shop/custemers/',
     {
-        method: 'POST',
-        headers: {
-            'Aithorization': 'Bearer ${BEARER_TOKEN}',
-            'Content-Type': 'application/json;charset=utf-8'
-          },
-        body: JSON.stringify(userRegistrationData)
-    })
+      method: 'POST',
+      headers: {
+        Aithorization: 'Bearer ${BEARER_TOKEN}',
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(userRegistrationData),
+    },
+  );
 
-    const result = await response.json();
-    alert(result.message);
-
+  const result = await response.json();
+  alert(result.message);
 }
 
 export default getUserObjectRegistrationPage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,4 +32,4 @@ export default defineConfig({
     //   plugins: [rollupNodePolyFill()],
     // },
   },
-})
+});
