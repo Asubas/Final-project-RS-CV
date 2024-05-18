21e8698cda2e2a8ba46429c5cc8e9d195c3f6cfb
Anton <lipan48@gmail.com>
2024-05-18 14:49:58 +0300

    RSS-ECOMM-2_20/feat: add redirection button on notFound page

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -1,16 +1,22 @@
 import './notFound.scss';
 import img404 from '../../assets/svg/404v2.svg';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
     <>
-      <section className="notFoundWrap">
-        <div className="notFoundCont">
-          <h2 className="notFoundHead">404</h2>
-          <p className="notFoundText">Looks like you were lost.</p>
-          <p className="notFoundText">There no page you looking for :(</p>
+      <section className="notFoundSection">
+        <div className="notFoundWrap">
+          <div className="notFoundCont">
+            <h2 className="notFoundHead">404</h2>
+            <p className="notFoundText">Looks like you were lost.</p>
+            <p className="notFoundText">There no page you looking for :(</p>
+          </div>
+          <img src={img404} alt="Page not found" className="notFoundImg" />
         </div>
-        <img src={img404} alt="Page not found" className="notFoundImg" />
+        <Link to="/" className="btn_black">
+          Go Home
+        </Link>
       </section>
     </>
   );
