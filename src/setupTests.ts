299bd9a8e212660fba244cde2161d9cf59e987fe
asubas <znaxor74@mail.ru>
2024-05-19 20:55:03 +0300

    ECOM-0/fix: change setup file for hide project keys

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom';
 
-process.env.VITE_CTP_CLIENT_ID = 'bNwUzGtwBgGmTmZVwdJSf22L';
-process.env.VITE_CTP_CLIENT_SECRET = 'vU45XItkf1dp00EXQBF9Xp3xDuR6b_hY';
-process.env.VITE_CTP_PROJECT_SCOPE = 'coffee-tea-shop';
-process.env.VITE_CTP_PROJECT_KEY = 'coffee-tea-shop';
-process.env.VITE_CTP_API_URL = 'https://api.europe-west1.gcp.commercetools.com';
-process.env.VITE_CTP_API_AUTH = 'https://auth.europe-west1.gcp.commercetools.com';
+process.env.VITE_CTP_CLIENT_ID = '';
+process.env.VITE_CTP_CLIENT_SECRET = '';
+process.env.VITE_CTP_PROJECT_SCOPE = '';
+process.env.VITE_CTP_PROJECT_KEY = '';
+process.env.VITE_CTP_API_URL = '';
+process.env.VITE_CTP_API_AUTH = '';
