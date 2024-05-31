89d5f1043f64ce30a9f3301407245de3653a909a
Anton <lipan48@gmail.com>
2024-05-31 18:20:11 +0300

    RSS-ECOMM-3_14/feat: add basic layout of component

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,5 +1,32 @@
+import { ApiRoot } from '@commercetools/platform-sdk';
+import './profile.scss';
+
 function Profile() {
-  return <h1>Profile page</h1>;
+
+  return (
+    <>
+      <section className="profileSection">
+        <div className="personalWrap">
+          <div className="personal">
+            <p className="personal_label">First name</p>
+            <p className="personal_value">Some first name</p>
+          </div>
+          <div className="personal">
+            <p className="personal_label">Last name</p>
+            <p className="personal_value">Some last name</p>
+          </div>
+          <div className="personal">
+            <p className="personal_label">e-mail</p>
+            <p className="personal_value">Some e-mail</p>
+          </div>
+          <div className="personal">
+            <p className="personal_label">Date of birth</p>
+            <p className="personal_value">Some date</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Profile;
