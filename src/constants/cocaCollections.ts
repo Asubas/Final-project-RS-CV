e861fa30367dab1498f85b42658af42a05607703
Леха <znaxor74@mail.ru>
2024-06-06 09:32:45 +0300

    fix: fix naming for working routing

--- src/constants/cocaCollections.ts
+++ src/constants/cocaCollections.ts
@@ -7,10 +7,10 @@ const subcategoriesCococa = ['Forastero', 'TRINITARIO', 'Criollo', 'All'];
 const backgroundImagesCocoa = [subCoca1, subCoca2, subCoca3, subCoca4];
 
 const cocoaUrl = {
-  'e633814a-6201-411d-9747-96efa44f7cb7': 'Forastero',
-  '597badfa-92b2-4fc0-9d55-9eab2d8bd6c1': 'TRINITARIO',
-  'c5dbdf54-cd6c-4be6-8ec6-bb65f0e513c9': 'Criollo',
-  '7a2657a3-ae01-452b-8e33-edb51503dceb': 'All',
+  'e633814a-6201-411d-9747-96efa44f7cb7': 'forastero',
+  '597badfa-92b2-4fc0-9d55-9eab2d8bd6c1': 'trinitario',
+  'c5dbdf54-cd6c-4be6-8ec6-bb65f0e513c9': 'criollo',
+  '7a2657a3-ae01-452b-8e33-edb51503dceb': 'all',
 };
 
 export { subcategoriesCococa, backgroundImagesCocoa, cocoaUrl };
--- src/constants/coffeeCollections.ts
+++ src/constants/coffeeCollections.ts
@@ -26,12 +26,12 @@ const backgroundImagesCoffee = [
 ];
 
 const coffeeUrl = {
-  '7baa2a11-d416-49cb-9645-4d0b50772c86': 'Brazilia',
-  'a84a4b2f-fd84-4c53-8da3-6e58be5551b5': 'Colombia',
-  '23c756e3-6674-4817-a3f5-568a6fc470d9': 'Kenya',
-  '9a2f447e-2a6b-498f-bb80-49816c810c6b': 'Ethiopia',
-  'c6333a6d-0dfe-431e-a178-ee2b52f6e8a1': 'CostaRica',
-  '133c8f18-e588-4524-b875-21a164bd0fbe': 'Guatemala',
-  '86625d6c-fcb0-4f8d-a58f-9f67cc8b13a4': 'All',
+  '7baa2a11-d416-49cb-9645-4d0b50772c86': 'brazilia',
+  'a84a4b2f-fd84-4c53-8da3-6e58be5551b5': 'colombia',
+  '23c756e3-6674-4817-a3f5-568a6fc470d9': 'kenya',
+  '9a2f447e-2a6b-498f-bb80-49816c810c6b': 'ethiopia',
+  'c6333a6d-0dfe-431e-a178-ee2b52f6e8a1': 'costaRica',
+  '133c8f18-e588-4524-b875-21a164bd0fbe': 'guatemala',
+  '86625d6c-fcb0-4f8d-a58f-9f67cc8b13a4': 'all',
 };
 export { subcategoriesCoffee, backgroundImagesCoffee, coffeeUrl };
