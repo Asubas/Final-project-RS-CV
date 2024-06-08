447346714ca74feb6823fe9fc4d947df85001d49
Anton <lipan48@gmail.com>
2024-06-08 16:48:25 +0300

    RSS-ECOMM-4_20/feat: implement rss block on about us page

--- src/pages/aboutUs/aboutUs.tsx
+++ src/pages/aboutUs/aboutUs.tsx
@@ -6,6 +6,7 @@ interface Developer {
   about: string;
   img: string | '';
   gitUrl: string;
+  role: string;
 }
 
 const developerData: Developer[] = [
@@ -16,6 +17,7 @@ const developerData: Developer[] = [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas aperiam ea quasi repellendus, inventore numquam ex laboriosam perferendis rerum facilis eaque tenetur expedita. Voluptatem illum, consequuntur laboriosam possimus ipsa officia!',
     img: '',
     gitUrl: 'https://github.com/Asubas',
+    role: 'teamlid, capitan',
   },
   {
     name: 'lipan48',
@@ -24,6 +26,7 @@ const developerData: Developer[] = [
       'My name is Anton, and I’m that guy who always seeks new challenges and is ready to learning something new. I’m 35 years old and have experience in sales and logistics, but I realized that I`m in search of myself. I see myself in front-end development. I’m self-motivated and always willing to learn new technologies and development procedures.',
     img: 'https://avatars.githubusercontent.com/u/120409900?s=400&u=bcd265e18b819ee7aa8f65a4a1aaa7428fd34b68&v=4',
     gitUrl: 'https://github.com/lipan4836',
+    role: 'jira-technic, solder',
   },
   {
     name: 'DzmitryAlekseev',
@@ -32,12 +35,23 @@ const developerData: Developer[] = [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas aperiam ea quasi repellendus, inventore numquam ex laboriosam perferendis rerum facilis eaque tenetur expedita. Voluptatem illum, consequuntur laboriosam possimus ipsa officia!',
     img: '',
     gitUrl: 'https://github.com/DzmitryAlekseev',
+    role: 'solder',
   },
 ];
 
 function AboutUs() {
   return (
     <section className="aboutUsSect">
+      <div className="rssWrap">
+        <div className="rssLogo">
+          <a href="https://rs.school/" className="rssLogo_link"></a>
+        </div>
+        <div className="rssDisc">
+          Do you want to dive into frontend development and learn how to create cool projects? Then
+          sign up for RS School!! Join ot our team!
+        </div>
+        <div className="rssTeam"></div>
+      </div>
       <div className="developerWrap">
         {developerData.map((developer, index) => (
           <div className="developer" key={index}>
@@ -58,6 +72,7 @@ function AboutUs() {
                 {developer.name}
               </a>
               <p className="developer_info__location">{developer.location}</p>
+              <p className="developer_info__location">{developer.role}</p>
               <p className="developer_info__about">{developer.about}</p>
             </div>
           </div>
