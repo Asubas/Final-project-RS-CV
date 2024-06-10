a96cd0d6b43a29df7af83d7a998462e2c6373c16
Anton <lipan48@gmail.com>
2024-06-10 17:19:15 +0300

    RSS-ECOMM-4_10/feat: implement layout of cart page with data

--- src/pages/myBag/myBag.tsx
+++ src/pages/myBag/myBag.tsx
@@ -3,6 +3,8 @@ import { Cart } from '@commercetools/platform-sdk';
 import { getCart } from '../../lib/flow/getCart';
 import './myBag.scss';
 import ItemInBag from './itemInBag';
+import LoadingSnippet from '../../components/loadingSnippet/loadingSnippet';
+import EmptyBag from './emptyBag';
 
 function MyBag() {
   const [cart, setCart] = useState<Cart | null>(null);
@@ -22,40 +24,49 @@ function MyBag() {
     fetchCart();
   }, []);
 
+  if (!cart) {
+    return <LoadingSnippet />;
+  }
+
+  if (cart.lineItems.length === 0) {
+    return <EmptyBag />;
+  }
+
+  const subtotal = (cart.totalPrice.centAmount / 100).toFixed(2);
+  const total = cart.totalPrice.centAmount / 100 + deliveryPrice;
+
   return (
     <section className="cartSection">
       <div className="cartWrap">
         <div className="itemsWrap">
           <ul className="itemsList">
-            {cart ? (
-              cart.lineItems.map((item) => (
-                <li key={item.id}>
-                  <ItemInBag item={item} />
-                </li>
-              ))
-            ) : (
-              <li>loading</li>
-            )}
+            {cart.lineItems.map((item) => (
+              <li className="itemCart" key={item.id}>
+                <ItemInBag item={item} />
+              </li>
+            ))}
           </ul>
+          <div className="summeryLine"></div>
+          <div className="subtotal">
+            <span className="subtotal_name">Subtotal</span>
+            <span className="subtotal_value">${subtotal}</span>
+          </div>
+          <button className="btn_white backToShop">back to shopping</button>
         </div>
         <div className="summeryList">
           <h2 className="summeryH2">Order summery</h2>
           <div className="pricesBlock">
             <span className="pricesBlock_name">Subtotal</span>
-            <span className="pricesBlock_value">
-              ${cart ? (cart.totalPrice.centAmount / 100).toFixed(2) : 0}
-            </span>
+            <span className="pricesBlock_value">${subtotal}</span>
           </div>
           <div className="pricesBlock">
             <span className="pricesBlock_name">Delivery</span>
-            <span className="pricesBlock_value">${deliveryPrice}</span>
+            <span className="pricesBlock_value">${deliveryPrice.toFixed(2)}</span>
           </div>
           <div className="summeryLine"></div>
           <div className="pricesBlock">
             <span className="pricesBlock_name name_total">Total</span>
-            <span className="pricesBlock_value value_total ">
-              ${(cart ? cart.totalPrice.centAmount / 100 : 0) + deliveryPrice}
-            </span>
+            <span className="pricesBlock_value value_total">${total.toFixed(2)}</span>
           </div>
           <span className="summeryShipping">Estimated shipping time: 2 days</span>
         </div>
