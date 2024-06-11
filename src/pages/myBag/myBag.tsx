891b92764ebef99d77034d060fdbb0158190c802
Леха <znaxor74@mail.ru>
2024-06-12 01:25:44 +0300

    ECOMM-40_RSS-ECOMM-4_11/fix: fix logic response cart page

--- src/pages/myBag/myBag.tsx
+++ src/pages/myBag/myBag.tsx
@@ -27,12 +27,18 @@ function MyBag() {
   useEffect(() => {
     const fetchCart = async () => {
       try {
-        const cartData = await getCart();
-        setCart(cartData.body);
-        const countProduct = cartData.body.totalLineItemQuantity;
-        if (countProduct && countRef.current && countProduct > 0) {
-          countRef.current.textContent = countProduct.toString();
-        }
+        let cartData;
+        setTimeout(async () => {
+          setTimeout(async () => {
+            cartData = await getCart();
+            setCart(cartData.body);
+            const countProduct = cartData.body.totalLineItemQuantity;
+            if (countProduct && countRef.current && countProduct > 0) {
+              countRef.current.textContent = countProduct.toString();
+              console.log(cartData.body);
+            }
+          }, 300);
+        }, 300);
       } catch (error) {
         console.error(error);
       }
